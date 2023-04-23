c197c5693d88649ca54875d06108a4d050471fc5
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2023-04-24 01:23:39 +0530

    add endpoints

--- rest_v1_calender/urls.py
+++ rest_v1_calender/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import GoogleCalendarRedirectView, GoogleCalendarInitView, GoogleCalenderHelloWorldView
+from .views import GoogleCalendarInitView, GoogleCalendarRedirectView
 
 
 urlpatterns = [
-    path('init/', GoogleCalendarInitView, name='calender_init'),
-    path('redirect/', GoogleCalendarRedirectView, name='calender_redirect'),
-    path('hello/', GoogleCalenderHelloWorldView, name='calender_hello_world'),
+    path('init/', GoogleCalendarInitView),
+    path('redirect/', GoogleCalendarRedirectView),
 ]
--- rest_v1_calender/views.py
+++ rest_v1_calender/views.py
@@ -1,15 +1,64 @@
-from django.shortcuts import render
+import datetime
+import os
+
 from django.http import HttpResponse
+from django.shortcuts import HttpResponseRedirect
+
+import google_apis_oauth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
-# Create your views here.
+# OAuth doesn't allow http unsecured connections for
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+CLIENT_ID_FILE = "credentials.json"
+BASE_URI = 'http://127.0.0.1:8000/rest/v1/calender/'
+REDIRECT_URI = BASE_URI + 'redirect/'
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
 def GoogleCalendarInitView(request):
-    pass
+    oauth_url = google_apis_oauth.get_authorization_url(
+        CLIENT_ID_FILE, SCOPES, REDIRECT_URI)
+    return HttpResponseRedirect(oauth_url)
+
+# Code taken from https://developers.google.com/calendar/api/quickstart/python
 
 
 def GoogleCalendarRedirectView(request):
-    pass
+    try:
+        # Get user credentials
+        creds = google_apis_oauth.get_crendentials_from_callback(
+            request,
+            CLIENT_ID_FILE,
+            SCOPES,
+            REDIRECT_URI
+        )
+        # creds_json = creds.to_json()
+    except:
+        return HttpResponse("Login failed / expired")
 
+    try:
+        service = build('calendar', 'v3', credentials=creds)
 
-def GoogleCalenderHelloWorldView(request):
-    return HttpResponse("Hello, world")
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            return HttpResponse('No upcoming events found.')
+
+        events_response = []
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            events_response.append(start + " " + event['summary'])
+
+    except HttpError:
+        return HttpResponse('Error occurred')
+
+    return HttpResponse(events_response)
